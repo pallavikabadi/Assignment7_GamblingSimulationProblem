fa4097bfcf4d11c82c9f22a6d61b76fac7816b52
pallavikabadi <58931162+pallavikabadi@users.noreply.github.com>
2023-05-02 12:47:26 +0530

    Added:Total Amount Won Or Lost After 20 Days

--- src/com/bridgelabz/GamblingSimulation.java
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -2,42 +2,60 @@ package com.bridgelabz;
 //As a Gambler, would start with a stake of $100 every day and bet $1 every game.
 
 public class GamblingSimulation {
+    static int stackEveryDay = 100;
+    static int betEveyGame = 1;
+    
     public static void main(String[] args) {
-       System.out.println(" Gambling Simulation Problem ");
+           System.out.println(" Gambling Simulation Problem ");
 
         //Initializing Stack of Every day and Bet Amount
 
         System.out.println("Stack of Every Day = " + stackEveryDay + "$");
         System.out.println("Bet Amount Every Game = " + betEveyGame + "$");
 
-                //Initializing 50% of the Everyday Stock Won and Lost
+
+        //Initializing 50% of the Everyday Stock Won and Lost
 
         int stackWon = 150;
         int stackLost = 50;
+        int totalAmount=0;
 
         //Gambler if won or lost 50% of the stake, would resign for the day (UC3)
 
+        for (int days = 1; days <= 20; days++) {
+            stackEveryDay = 100;
+
         while (stackEveryDay > stackLost && stackEveryDay < stackWon) {
 
-             // Gambler make $1 bet either win or loose $1 (UC2).
-             //Using Random to find the Bet wins or lose.
+            // Gambler make $1 bet either win or loose $1 (UC2).
+            //Using Random to find the Bet wins or lose.
 
             Random random = new Random();
             int bet = random.nextInt(2);
-            if (bet == 1) {
-                System.out.println("Gambler Bet WINS");
 
-              //When the Bet wins Bet amount is added to Stack of Everyday
+            switch (bet) {
+                case 1:
+                    //When the Bet wins Bet amount is added to Stack of Everyday
+                    stackEveryDay += betEveyGame;
+                    break;
+                default:
+                       
+                     //When the Bet loose Stack of Every day is deducted by bet amount
+                        
+                       stackEveryDay -= betEveyGame;
+                       break;
 
-                stackEveryDay += betEveyGame;
-                System.out.println("Final Stack when Bet Wins = " + stackEveryDay + "$");
-            } else {
-                System.out.println("Gambler Bet Lose");
-                //When the Bet loose Stack of Every day is deducted by bet amount
-
-                stackEveryDay -= betEveyGame;
-                System.out.println("Final Stack when Bet Lose = " + stackEveryDay + "$");
             }
         }
-    }
+
+            if(stackEveryDay == stackWon) {
+                System.out.println("Player Has Won Gambler For Day "+days);
+                totalAmount += stackWon;
+            }else {
+                System.out.println("Player Has Loss Gambler For Day "+days);
+                totalAmount -= stackLost;
+            }
+            System.out.println("Total Win Price: "+totalAmount + "$");
+            }
+        }
 }
