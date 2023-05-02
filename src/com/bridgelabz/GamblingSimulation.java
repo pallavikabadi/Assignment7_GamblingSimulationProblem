c144fa0d35a7bba0846565fab91e79ca619b1316
pallavi kabadi <pallavi08jadhav@gmail.com>
2023-05-02 09:14:17 +0530

    Added:Gambling Simulation Problem

---
+++ src/com/bridgelabz/GamblingSimulation.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+//As a Gambler, would start with a stake of $100 every day and bet $1 every game.
+public class GamblingSimulation {
+    public static void main(String[] args) {
+        System.out.println(" Gambling Simulation Problem ");
+
+        //Initializing Stack of Every day and Bet Amount
+        int stackEveryDay = 100;
+        int betEveyGame = 1;
+
+        System.out.println("Stack of Every Day = " + stackEveryDay + "$");
+        System.out.println("Bet Amount Every Game = " + betEveyGame + "$");
+    }
+}
